30509183611e24e54aaa57de098762243358aa8e
dimsssss <caruen@hanmail.net>
2022-09-23 17:42:03 +0900

    feat: 인증 코드 생성 컨트롤러

---
+++ authorization/authorizationController.js
@@ -0,0 +1,18 @@
+const {StatusCodes} = require('http-status-codes')
+const authorizationService = require('./authorizationService')
+
+const getAuthorizeCode = async (validator, req, res, next) => {
+  try {
+    if (Object.hasOwn(validator, 'error')) {
+      return res.status(StatusCodes.BAD_REQUEST).send(validator.error.message)
+    }
+    const code = await authorizationService.generateCode(validator.value)
+    return res.status(StatusCodes.CREATED).send({code})
+  } catch (err) {
+    return res.status(err.statusCode).send(err.message)
+  }
+}
+
+module.exports = {
+  getAuthorizeCode,
+}
