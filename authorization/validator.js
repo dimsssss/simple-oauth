cf3ae7dbd9e0784116a6d32ce31befd675ba6acc
dimsssss <caruen@hanmail.net>
2022-09-23 17:42:34 +0900

    feat: 인증 코드 생성 검증 코드

--- authorization/validator.js
+++ authorization/validator.js
@@ -1,33 +1,18 @@
 const Joi = require('joi')
 
-const schema = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required(),
-
-  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-
-  repeat_password: Joi.ref('password'),
-
-  access_token: [Joi.string(), Joi.number()],
-
-  birth_year: Joi.number().integer().min(1900).max(2013),
-
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: {allow: ['com', 'net']},
-  }),
+const codeRequestSchema = Joi.object({
+  clientId: Joi.number().min(1).required(),
+  grantType: Joi.string().equal('authorization_code').required(),
+  redirectUrl: Joi.string().uri().required(),
+  codeChallenge: Joi.string().base64().required(),
+  codeChallengeMethod: Joi.string().equal('sha256').required(),
 })
-  .with('username', 'birth_year')
-  .xor('password', 'access_token')
-  .with('password', 'repeat_password')
 
-schema.validate({username: 'abc', birth_year: 1994})
-// -> { value: { username: 'abc', birth_year: 1994 } }
+const validateCodeRequest = async (req, res, next) => {
+  const value = await codeRequestSchema.validate(req.body)
+  return next(value, req, res, next)
+}
 
-schema.validate({})
-// -> { value: {}, error: '"username" is required' }
-
-// Also -
-
-try {
-  const value = await schema.validateAsync({username: 'abc', birth_year: 1994})
-} catch (err) {}
+module.exports = {
+  validateCodeRequest,
+}
