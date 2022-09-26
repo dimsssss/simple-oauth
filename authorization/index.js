c7580b70be7a84cbf5428f0bf5c16687b404687f
dimsssss <caruen@hanmail.net>
2022-09-26 20:00:32 +0900

    feat: router 추가

--- authorization/index.js
+++ authorization/index.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {validateCodeRequest} = require('./validator')
-const authorizationController = require('./authorizationController')
+const {validateCodeRequest, validateAccessTokenRequest} = require('./presentation/validator')
+const authorizationController = require('./presentation/authorizationController')
 
-router.post(
-  '/code',
-  validateCodeRequest,
-  authorizationController.getAuthorizeCode,
-)
+router.post('/code', validateCodeRequest, authorizationController.getAuthorizeCode)
+router.post('/token', validateAccessTokenRequest, authorizationController.getAccessToken)
 
 module.exports = router
